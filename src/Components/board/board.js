ac4edbf4b8535300f75cc47b04c38f76464707c1
Edel David <salazaredeldavid@gmail.com>
2021-03-28 18:00:52 +0200

    updated hover to be the ship

--- src/Components/board/board.js
+++ src/Components/board/board.js
@@ -14,6 +14,7 @@ const Board = () => {
     const [selectedShip, setSelectedShip] = useState("carrier")
     const [axis, setAxis] = useState("x")
     const [hoveredCell, setHoveredCell] = useState(null)
+    const [isAllow, setIsAllow] = useState(true)
  
     const [carrierStartPosition, setCarrierStartPosition] = useState(null)
     const [carrierAxis, setCarrierAxis] = useState("")
@@ -33,36 +34,98 @@ const Board = () => {
     const handleCellClick = (n) => {
         if(selectedShip==="carrier"){
             if(isAllowed(n, 5, axis)){
+                setSelectedShip("battleship")
+            }
+         }else if (selectedShip==="battleship"){
+            if(isAllowed(n, 4, axis)){
+                setSelectedShip("destroyer")
+            }
+        }else if (selectedShip==="destroyer"){
+            if(isAllowed(n, 3, axis)){
+                setSelectedShip("submarine")
+            }
+        }else if (selectedShip==="submarine"){
+            if(isAllowed(n, 3, axis)){
+                setSelectedShip("patrol")
+            }
+        }else if(selectedShip==="patrol"){
+            if(isAllowed(n, 2, axis)){
+                setSelectedShip("")
+            }
+        }
+    }
+    const handleCellHover = (n) => {
+        setHoveredCell(n)
+        if(selectedShip==="carrier"){
+            if(isAllowed(n, 5, axis)){
                 setCarrierAxis(axis)
                 setCarrierStartPosition(n)
-                setSelectedShip("battleship")
+                setIsAllow(true)
+            }else{
+                setCarrierAxis("")
+                setCarrierStartPosition(null)
+                setIsAllow(false)
             }
          }else if (selectedShip==="battleship"){
             if(isAllowed(n, 4, axis)){
                 setBattleshipAxis(axis)
                 setBattleshipStartPosition(n) 
-                setSelectedShip("destroyer")
+                setIsAllow(true)
+            }else{
+                setBattleshipAxis("")
+                setBattleshipStartPosition(null) 
+                setIsAllow(false)
             }
         }else if (selectedShip==="destroyer"){
             if(isAllowed(n, 3, axis)){
                 setDestroyerAxis(axis)
                 setDestroyerStartPosition(n) 
-                setSelectedShip("submarine")
+                setIsAllow(true)
+            }else{
+                setDestroyerAxis("")
+                setDestroyerStartPosition(null) 
+                setIsAllow(false)
             }
         }else if (selectedShip==="submarine"){
             if(isAllowed(n, 3, axis)){
                 setSubmarineAxis(axis)
                 setSubmarineStartPosition(n) 
-                setSelectedShip("patrol")
+                setIsAllow(true)
+            }else{
+                setSubmarineAxis("")
+                setSubmarineStartPosition(null) 
+                setIsAllow(false)
             }
         }else if(selectedShip==="patrol"){
             if(isAllowed(n, 2, axis)){
                 setPatrolAxis(axis)
                 setPatrolStartPosition(n) 
-                setSelectedShip("")
+                setIsAllow(true)
+            }else{
+                setPatrolAxis("")
+                setPatrolStartPosition(null) 
+                setIsAllow(false)
             }
         }
     }
+    const handleCellLeave = (n) => {
+        setHoveredCell(null)
+        if(selectedShip==="carrier"){
+            setCarrierStartPosition(null)
+        }else if (selectedShip==="battleship"){
+            setBattleshipStartPosition(null) 
+        }else if (selectedShip==="destroyer"){
+            setDestroyerStartPosition(null) 
+        }else if (selectedShip==="submarine"){
+            setSubmarineStartPosition(null) 
+        }else if(selectedShip==="patrol"){
+            setPatrolStartPosition(null) 
+        }
+    }
+
+    const notAllowedStyles = (hoveredCell, i) => {
+        if(!isAllow && hoveredCell === i) return {backgroundColor: "#80000078"}
+    } 
 
     return (<>
         <div onClick={changeAxis}>AXIS: {axis}</div>
@@ -84,9 +147,9 @@ const Board = () => {
                                 className="cell" 
                                 key={i}
                                 onClick={()=>handleCellClick(i)}
-                                onMouseEnter={()=>setHoveredCell(i)}
-                                onMouseLeave={()=>setHoveredCell(null)}
-                                style={hoverStyles(i, hoveredCell, selectedShip, axis)}
+                                onMouseEnter={()=>handleCellHover(i)}
+                                onMouseLeave={()=>handleCellLeave(i)}
+                                style={notAllowedStyles(hoveredCell, i)}
                             />
                         ))
                     }
